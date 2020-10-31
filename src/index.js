630827f0f618c0cce06f085797178d22aa8fe176
Josh Murr <joshuadavidmurr@gmail.com>
2020-10-31 15:18:14 +0000

    Basic setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles.scss';
+
+const ctx = document.getElementById('output').getContext('2d');
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, 512, 512);
---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Pix2Pix Demo',
+      template: 'src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
